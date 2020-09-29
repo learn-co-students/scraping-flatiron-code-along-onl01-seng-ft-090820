a600c480f9167230213254518deb86bd6c06209e
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-29 21:48:13 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  #every time an instance of the class is initialized, we should push it into the @@all class variable
+  @@all =[]
 
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
+end 
 
